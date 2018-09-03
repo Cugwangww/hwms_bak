66ca0328d257a874592354ea630220cce5d168db
wwwang7 <wwwang7@iflytek>
2018-09-03 22:08:20 +0800

    init Project

--- src/main/java/com/spring/iflytek/ServletInitializer.java
+++ src/main/java/com/spring/iflytek/ServletInitializer.java
@@ -1,4 +1,4 @@
-package com.spring.springtest;
+package com.spring.iflytek;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(SpringTestApplication.class);
+        return application.sources(com.spring.iflytek.SpringTestApplication.class);
     }
 
 }
--- src/main/java/com/spring/iflytek/SpringTestApplication.java
+++ src/main/java/com/spring/iflytek/SpringTestApplication.java
@@ -1,10 +1,11 @@
-package com.spring.springtest;
+package com.spring.iflytek;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringTestApplication {
+
 
     public static void main(String[] args) {
         SpringApplication.run(SpringTestApplication.class, args);
