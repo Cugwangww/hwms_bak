29322c2de1b040c4a32672e71daedb44eda6f404
879954889@qq.com <879954889@qq.com>
2024-04-22 10:23:53 +0800

    replace

---
+++ src/main/java/com/动态规划/三角形最小路径和/Solution.java
@@ -0,0 +1,43 @@
+package com.动态规划.三角形最小路径和;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author author
+ * @description
+ * @date 2024/4/14
+ */
+public class Solution {
+
+    /**
+     * https://leetcode.cn/problems/triangle/description/
+     * 给定一个三角形 triangle ，找出自顶向下的最小路径和。
+     * 每一步只能移动到下一行中相邻的结点上。相邻的结点 在这里指的是 下标 与 上一层结点下标 相同或者等于 上一层结点下标 + 1 的两个结点。也就是说，如果正位于当前行的下标 i ，那么下一步可以移动到下一行的下标 i 或 i + 1 。
+     */
+    public static int minimumTotal(List<List<Integer>> triangle) {
+        int length = triangle.size();
+        int[][] dp = new int[length][length];
+        dp[0][0] = triangle.get(0).get(0);
+        for (int i = 1; i < length; i++) {
+            dp[i][0]=dp[i-1][0]+triangle.get(i).get(0);
+            for (int j = 1; j < i; j++) {
+                dp[i][j] = Math.min(dp[i-1][j-1],dp[i-1][j]) + triangle.get(i).get(j);
+            }
+            dp[i][i]=dp[i-1][i-1]+triangle.get(i).get(i);
+        }
+        int res = dp[length-1][0];
+        for (int i = 0; i < length; i++) {
+            res = Math.min(res,dp[length-1][i]);
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        List<List<Integer>> triangle = new ArrayList<>();
+        triangle.add(Arrays.asList(-1));
+        triangle.add(Arrays.asList(-2,-3));
+        System.out.println(minimumTotal(triangle));
+    }
+}
--- src/main/java/com/动态规划/最小路径和/MinPathSum.java
+++ src/main/java/com/动态规划/最小路径和/MinPathSum.java
@@ -26,17 +26,20 @@ public class MinPathSum {
      * @return
      */
     public static int minPathSum(int[][] grid) {
-        int m = grid.length, n = grid[0].length;
-        int[][] dp = new int[m][n];
-        dp[0][0] = grid[0][0];
-        for(int j = 1; j < n; j++) dp[0][j] = dp[0][j - 1] + grid[0][j];
-        for(int i = 1; i < m; i++){
-            dp[i][0] = dp[i - 1][0] + grid[i][0];
-            for(int j = 1; j < n; j++){
-                dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if(i ==0 && j ==0){
+                    continue;
+                }else if(i ==0){
+                    grid[i][j] = grid[i][j] + grid[i][j-1];
+                }else if(j==0){
+                    grid[i][j] = grid[i][j] + grid[i-1][j];
+                }else {
+                    grid[i][j] = grid[i][j] + Math.min(grid[i-1][j],grid[i][j-1]);
+                }
             }
         }
-        return dp[m - 1][n - 1];
+        return grid[grid.length-1][grid[0].length-1];
     }
 
     public static void main(String[] args) {
